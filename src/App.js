bfb3cc40c864a59c11ac1b4e304d9b23cd1a8efa
unknown <mfatihcandan@gmail.com>
2021-02-04 11:56:24 +0300

    add css upts

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function SignIn() {
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      <p id="welcome-text">Welcome to our Chat Channel!</p>
     </>
   )
 
