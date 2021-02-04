9f38617ed4fd2840d1d3c748913ce161e38fd404
unknown <mfatihcandan@gmail.com>
2021-02-04 11:26:01 +0300

    initialize react & hooks & firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAywu_MplIZwlwycv9yW0l0g3MXT_qcjvg",
+  authDomain: "chatroomapp-react.firebaseapp.com",
+  projectId: "chatroomapp-react",
+  storageBucket: "chatroomapp-react.appspot.com",
+  messagingSenderId: "532513539929",
+  appId: "1:532513539929:web:322e605740624bf03c6e48"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
 function App() {
   return (
